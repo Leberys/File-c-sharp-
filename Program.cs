50e050e47995269d789c45c805dc12cbba2dd9a7
Den <den.yats@gmail.com>
2023-11-25 12:33:13 +0200

    Tasks

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program
+{
+    class MyClass
+    {
+        static void Main()
+        {
+            Console.Write("Введіть Ім'я : ");
+            string a = Convert.ToString(Console.ReadLine());
+            Console.Write("Введіть Прізвище : ");
+            string b = Convert.ToString(Console.ReadLine());
+            if (a == "Vova" && b == "Mihailenko")
+            {
+                Console.WriteLine("Зачекайте...................");
+                Console.Write("Successful");
+            }
+            else
+            {
+                Console.Write("FAILED");
+            }
+        }
+    }
+}
